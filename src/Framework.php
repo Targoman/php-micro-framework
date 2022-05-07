e9618f9e2336855e04752bc2c24c1dd831694ca0
kambizzandi <kambizzandi@gmail.com>
2022-05-07 17:35:17 +0430

    init

---
+++ src/Framework.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @author: Kambiz Zandi <kambizzandi@gmail.com>
+ */
+
+class Framework {
+    public static $baseNamespaces = [];
+    public static $autoloadMap = [];
+
+    public static function autoload($_className) {
+        if (strpos($_className, "\\") !== false) {
+			$name = str_replace("\\", "/", $_className) . ".php";
+            if (strpos($name, "/") === 0)
+				$name = substr($name, 1);
+
+            $ret = array_filter(array_map(
+                function($value) use ($name) {
+                    if (strpos($name, $value . "/") !== 0)
+                        return "";
+                    return $value;
+                },
+                self::$baseNamespaces));
+            if (empty($ret)) //not found
+				return;
+
+			foreach (static::$autoloadMap as $k => $v) {
+				$k = str_replace("\\", "/", $k);
+				$p = strpos($name, $k);
+				if ($p === 0) {
+					$name = $v . "/" . substr($name, strlen($k));
+					break;
+				}
+			}
+
+			$classFile = $name;
+			if ($classFile === false || !is_file($classFile))
+                return;
+		} else
+			return;
+
+		include $classFile;
+
+		if (!class_exists($_className, false) && !interface_exists($_className, false) && !trait_exists($_className, false))
+			throw new \Exception("Unable to find '$_className' in file: $classFile. Namespace missing?");
+    }
+
+    public static function instantiateClass($_config) {
+        if (empty($_config["class"]))
+            throw new \Exception("class item not defined in config array");
+
+        $className = $_config["class"];
+        unset($_config["class"]);
+
+        // foreach ($_config as $k => &$v) {
+        //     if (isset($v["class"]))
+        //         $v["class"] = static:: instantiateClass($v);
+        // }
+
+        $reflector = new \ReflectionClass($className);
+        $class = $reflector->newInstance(); //WithoutConstructor();
+        unset($reflector);
+
+        foreach($_config as $prop => $value) {
+            $class->$prop = $value;
+        }
+
+        if (method_exists($class, "init"))
+            $class->init();
+
+        return $class;
+    }
+
+    // public static function instantiateClassByConfigName($_config, $_configName) {
+    //     if (empty($_config[$_configName]))
+    //         throw new \Exception("$_configName not configured");
+
+    //     $object = self::instantiateClass($_config[$_configName]);
+
+    //     if (is_null($object))
+    //         throw new \Exception("Could not create $_configName");
+
+    //     return $object;
+    // }
+
+}
+
+spl_autoload_register(["Framework", "autoload"], true, false);
+Framework::$autoloadMap = array_replace_recursive(
+    [
+        "Framework" => __DIR__
+    ],
+    require(__DIR__ . "/../../../../autoload.php")
+);
+krsort(Framework::$autoloadMap);
+
+foreach (Framework::$autoloadMap as $k => $v) {
+    Framework::$baseNamespaces[] = explode('\\', $k)[0];
+}
---
+++ src/db/MySql.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @author: Kambiz Zandi <kambizzandi@gmail.com>
+ */
+
+namespace Framework\db;
+
+use PDO;
+
+class MySql {
+    public $host = "127.0.0.1";
+    public $port = "3306";
+    public $username;
+    public $password;
+    public $schema;
+
+    static $PDO = null;
+
+    public function init() {
+        if (empty($this->username))
+            throw new \Exception("username is empty");
+        if (empty($this->password))
+            throw new \Exception("password is empty");
+        if (empty($this->schema))
+            throw new \Exception("schema is empty");
+    }
+
+    private function getDSN() {
+        return "mysql:host={$this->host};port={$this->port};dbname={$this->schema}";
+    }
+
+    public function getPDO() {
+        if (self::$PDO == null) {
+            self::$PDO = new \PDO($this->getDSN(), $this->username, $this->password);
+            self::$PDO->exec('SET NAMES "utf8mb4"');
+        }
+
+        return self::$PDO;
+    }
+
+    public function createCommand($_qry, $_params=[]) {
+        return new Command($this, $_qry, $_params);
+    }
+
+    public function selectAll($_qry, $_params=[]) {
+        $command = $this->createCommand($_qry, $_params);
+
+        return $command->queryAll();
+    }
+
+    public function selectOne($_qry, $_params=[]) {
+        $command = $this->createCommand($_qry, $_params);
+
+        return $command->queryOne();
+    }
+
+    public function execute($_qry, $_params=[]) {
+        $command = $this->createCommand($_qry, $_params);
+
+        return $command->execute();
+    }
+
+}
