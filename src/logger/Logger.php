f9431066e2e740b4b04ce9a43daab2fa35170b2a
kambizzandi <kambizzandi@gmail.com>
2022-05-14 17:39:24 +0430

    add logger

--- src/logger/Logger.php
+++ src/logger/Logger.php
@@ -11,13 +11,17 @@ class Logger
 {
     use \Targoman\Framework\core\ComponentTrait;
 
-    public function formatLog($_message) {
-        $out = '[' . date('Y/m/d H:i:s') . '] ' . $_message;
+    public function formatLog($_category, $_message) {
+        $out = '[' . date('Y/m/d H:i:s') . '] ' . "[$_category] " . $_message;
         return $out;
     }
 
     public function log($_message) {
-        echo $this->formatLog($_message) . "\n";
+        echo $this->formatLog('INFO', $_message) . "\n";
+    }
+
+    public function logException(\Throwable $_exp) {
+        echo $this->formatLog('EXCEPTION', $_exp->getMessage()) . "\n";
     }
 
 }
